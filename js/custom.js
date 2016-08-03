890123b7d688b36bb53de95ccdfc38b980d564c4
ov3rk1ll <ov3rk1ll@users.noreply.github.com>
2016-08-03 08:54:00 +0200

    add autolink for download

--- js/custom.js
+++ js/custom.js
@@ -40,6 +40,8 @@
 		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
 		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
 	});
+	
+	$.getJSON('/update.json', function(response){ $('a.apk-download').attr('href', response.apk); });
 
 	
 })(jQuery);
